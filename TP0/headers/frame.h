308f6b871ba75ac0d739f9062fa32b265f80eee2
PapaArt <artur.papa@ufv.br>
2022-09-08 09:19:18 -0300

    Scope of the functions

---
+++ TP0/headers/frame.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#ifndef FRAME_H
+#define FRAME_H
+
+void printFrame();
+void asterisk();
+void crossAsterisk();
+void XAsterisk();
+void randomArt();
+void jordan();
+
+#endif
