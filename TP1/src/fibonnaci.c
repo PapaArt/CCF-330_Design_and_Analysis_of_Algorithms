56a4415637a9e8b29e00efa5cc8036797a463d02
GuilhermeFCO <guilherme.f.castro@ufv.br>
2022-11-13 10:49:28 -0300

    limpando função fibonnaci

--- TP1/src/fibonnaci.c
+++ TP1/src/fibonnaci.c
@@ -32,17 +32,4 @@ void printFibonnaci(int* sequence, int n) {
 		printf("\n");
         count++;
     }
-	while (flag)
-	{
-		for (int j = 0; j < count; j++)	{
-			if (i == n) {
-				flag = 0;
-				break;
-			}
-			if (j < 2) sequence[i] = 1;
-			else sequence[i] = sequence[i-1] + sequence[i-2];
-			i++;
-		}
-		count++;
-	}
 }
